f947acf3f203f74d6509ed9bf3703ed03947b95b
Michal Korman <michal.korman@icloud.com>
2023-11-11 01:11:40 +0100

    define project structure

---
+++ src/test/java/com/github/mkorman9/roomoccupancymanager/StubTest.java
@@ -0,0 +1,11 @@
+package com.github.mkorman9.roomoccupancymanager;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+@QuarkusTest
+class StubTest {
+    @Test
+    public void stubTest() {
+    }
+}
